5d80f8f307b92d2a5b82a93548ae122e8d2d2285
stephane-klein <contact@stephane-klein.info>
2025-05-27 11:53:55 +0200

    WIP issue-alb-150

---
+++ src/routes/version/+page.svelte
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <style>
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+
+<body>
+    <ul>
+        <li>branch: <a href="https://github.com/etalab-ia/albert-conversation/tree/issue-alb-150/">"issue-alb-150"</a></li>
+        <li>gitDate": 2025-05-27_11:44:50_+0200</li>
+        <li>buildStamp: 2025-05-27_11:53:37-CEST</li>
+        <li>gitHash: <a href="https://github.com/etalab-ia/albert-conversation/tree/582d5aa0909dc3c18a39a000542fa539c32ddbe4">582d5aa0909dc3c18a39a000542fa539c32ddbe4</li>
+    </ul>
+</body>
+</html>
